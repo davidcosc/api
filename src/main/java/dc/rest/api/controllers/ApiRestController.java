bf0d55ba03774a3ae9dfb7ab134dd5220e492c5d
david <d.cornel@tu-braunschweig.de>
2018-07-12 21:26:00 +0200

    givenAGetRequestToServicesUri_whenGetServicesIsCalled_thenReturnListOfExistingServices as well as most simple implementation

--- src/main/java/dc/rest/api/controllers/ApiRestController.java
+++ src/main/java/dc/rest/api/controllers/ApiRestController.java
@@ -1,7 +1,21 @@
 package dc.rest.api.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
 /**
  * Created by david on 12.07.2018.
  */
+@RestController
 public class ApiRestController {
+    @GetMapping("/services")
+    public ResponseEntity<?> getServices() {
+        ArrayList<String> services = new ArrayList<String>();
+        services.add("service1");
+        services.add("service2");
+        return ResponseEntity.ok().body(services);
+    }
 }
--- src/test/java/dc/rest/api/controllers/ApiRestControllerTest.java
+++ src/test/java/dc/rest/api/controllers/ApiRestControllerTest.java
@@ -2,10 +2,15 @@ package dc.rest.api.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by david on 12.07.2018.
@@ -19,5 +24,17 @@ public class ApiRestControllerTest {
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders.standaloneSetup(new ApiRestController()).setControllerAdvice(new RestControllerAdvice()).build();
         objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    public void givenAGetRequestToServicesUri_whenGetServicesIsCalled_thenReturnListOfExistingServices() throws Exception {
+        //given
+        ArrayList<String> services = new ArrayList<String>();
+        services.add("service1");
+        services.add("service2");
+        //when
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/services")).andReturn().getResponse();
+        System.out.println(objectMapper.writeValueAsString(services) + " ::: " + response.getContentAsString());
+        assertEquals(objectMapper.writeValueAsString(services), response.getContentAsString());
     }
 }
